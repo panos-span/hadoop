7cc3f5ec20cf353b610935c1f4757ff8ceb13950
Panagiotis Spanakis <t8200158@aueb.gr>
2023-04-20 21:25:58 +0300

    Update run.ps1 and reducer.py

--- run.ps1
+++ run.ps1
@@ -1,15 +1,15 @@
 $i = 0
 while ($true) {
-    hadoop jar ../../../../usr/lib/hadoop-mapreduce/hadoop-streaming.jar -file centers.txt -file ./mapper.py -mapper ./mapper.py -file ./reducer.py -reducer ./reducer.py -input /testMapReduce/dataset -output "/testMapReduce/mapreduce-output$i"
+    hadoop jar C:\hadoop\share\hadoop\tools\lib\hadoop-streaming-3.3.0.jar -file centers.txt -file ./mapper.py -mapper ./mapper.py -file ./reducer.py -reducer ./reducer.py -input /input/data.txt -output "/testMapReduce/mapreduce-output$i"
     Remove-Item centers.txt
     hadoop fs -copyToLocal "/testMapReduce/mapreduce-output$i/part-00000" centers.txt
     $seeiftrue = Select-String -Path centers.txt -Pattern "Converged"
     if ($seeiftrue) {
-        Remove-Item centroids.txt
+        Remove-Item centers.txt
         hadoop fs -copyToLocal "/testMapReduce/mapreduce-output$i/part-00000" centers.txt
         break
     } else {
-        Remove-Item centroids.txt
+        Remove-Item centers.txt
         hadoop fs -copyToLocal "/testMapReduce/mapreduce-output$i/part-00000" centers.txt
     }
     $i++
